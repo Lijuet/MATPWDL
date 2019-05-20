be925e8710e12019bd97ceeb854d089eaf630f07
Lijuet <cotpdl1@gmail.com>
2019-05-20 15:47:23 +0900

    0518/selee/Intent move & FindViewVyID -KnockMain -> MyKnock -KnockMain -> SendKnock

--- app/src/main/java/edu/skku/map/matpwdl/KnockMainActivity.java
+++ app/src/main/java/edu/skku/map/matpwdl/KnockMainActivity.java
@@ -1,13 +1,44 @@
 package edu.skku.map.matpwdl;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class KnockMainActivity extends AppCompatActivity {
-
+    ListView lvMainKnockList, lvMyKnockList;
+    FloatingActionButton btnMakeKnock;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_knock_main);
+
+        //find View by ID
+        lvMainKnockList = findViewById(R.id.listView_MainKnockList);
+        lvMyKnockList = findViewById(R.id.listView_MyKnockList);
+        btnMakeKnock = findViewById(R.id.button_MakeKnock);
+
+        //if we click the list my knock list, go to MyKnockActivity
+        lvMyKnockList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentMyKnock = new Intent(KnockMainActivity.this, MyKnockActivity.class);
+                startActivity(intentMyKnock);
+            }
+        });
+
+        //if we click button, go to SendKnockActivity
+        btnMakeKnock.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentSendKnock = new Intent(KnockMainActivity.this, SendKnockActivity.class);
+                startActivity(intentSendKnock);
+            }
+        });
+
+
     }
 }
--- app/src/main/java/edu/skku/map/matpwdl/SendKnockActivity.java
+++ app/src/main/java/edu/skku/map/matpwdl/SendKnockActivity.java
@@ -2,12 +2,36 @@ package edu.skku.map.matpwdl;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Switch;
 
 public class SendKnockActivity extends AppCompatActivity {
-
+    EditText etMyKnockReciever, etMyKnockContent;
+    RadioButton btnOpt1_Im, btnOpt2_3m, btnOpt3_5m;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_send_knock);
+
+        etMyKnockReciever = findViewById(R.id.editText_MyKnockReciever);
+        etMyKnockContent = findViewById(R.id.editText_MyKnockContent);
+        btnOpt1_Im = findViewById(R.id.btnOpt1_im);
+        btnOpt2_3m = findViewById(R.id.btnOpt2_3m);
+        btnOpt3_5m = findViewById(R.id.btnOpt3_5m);
+
+        btnOpt1_Im.setOnClickListener(optionOnClickLister);
+        btnOpt2_3m.setOnClickListener(optionOnClickLister);
+        btnOpt3_5m.setOnClickListener(optionOnClickLister);
+
     }
+
+    RadioButton.OnClickListener optionOnClickLister = new RadioButton.OnClickListener(){
+        @Override
+        public void onClick(View v) {
+            //Depending on radion button option check, determine when a knock will send
+        }
+    };
 }
