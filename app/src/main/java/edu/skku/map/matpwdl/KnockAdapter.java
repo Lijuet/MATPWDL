ff431a80d4f30d3dce17876d2959f8446c6c7472
Lijuet <cotpdl1@gmail.com>
2019-05-25 08:05:44 +0900

    0525/selee/displaly sample DB data on main Knock Activity

--- app/src/main/java/edu/skku/map/matpwdl/KnockAdapter.java
+++ app/src/main/java/edu/skku/map/matpwdl/KnockAdapter.java
@@ -1,30 +1,32 @@
 package edu.skku.map.matpwdl;
 
+import android.content.ClipData;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class KnockAdapter extends BaseAdapter {
     LayoutInflater inflater;
-    private ArrayList<Knock> items;
+    private ArrayList<Knock> knocks;
 
-    public KnockAdapter (Context context, ArrayList<Knock> memos) {
+    public KnockAdapter(Context context, ArrayList<Knock> knocks) {
         this.inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        this.items = memos;
+        this.knocks = knocks;
     }
 
     @Override
     public int getCount() {
-        return items.size();
+        return knocks.size();
     }
 
     @Override
     public Knock getItem(int i) {
-        return items.get(i);
+        return knocks.get(i);
     }
 
     @Override
@@ -38,6 +40,15 @@ public class KnockAdapter extends BaseAdapter {
             view = inflater.inflate(R.layout.knock_item_layout, viewGroup, false);
         }
 
+        Knock item = knocks.get(i);
+
+        TextView receiever_tv = (TextView)view.findViewById(R.id.textView_KnockItemReciever);
+        TextView contents_tv = (TextView)view.findViewById(R.id.textView_KnockItemContent);
+        TextView date_tv = (TextView)view.findViewById(R.id.textView_KnockItemDate);
+
+        receiever_tv.setText(item.getReceiver());
+        contents_tv.setText(item.getContent());
+        date_tv.setText(item.getDate());
 
         return view;
     }
