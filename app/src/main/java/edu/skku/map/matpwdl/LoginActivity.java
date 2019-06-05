a810af138b6708b799e6e9426ea7e8e795c580d8
Lijuet <cotpdl1@gmail.com>
2019-06-05 19:53:23 +0900

    0605/selee/Knock메세지 보내기 완료

--- app/src/main/java/edu/skku/map/matpwdl/LoginActivity.java
+++ app/src/main/java/edu/skku/map/matpwdl/LoginActivity.java
@@ -2,6 +2,7 @@ package edu.skku.map.matpwdl;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //todo 메세지에서 login 화면으로 올때 기존 메세지 화면 종료시키기 및 그냥 켰을 때 오류 안나는지?
     }
 }
