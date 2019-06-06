1dd9276dc80cb79b04f4448df51bde1eab0d57fa
fjaoief <38908138+fjaoief@users.noreply.github.com>
2019-06-07 01:44:48 +0900

    login 수정

--- app/src/main/java/edu/skku/map/matpwdl/LoginActivity.java
+++ app/src/main/java/edu/skku/map/matpwdl/LoginActivity.java
@@ -10,25 +10,56 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class LoginActivity extends AppCompatActivity {
     EditText IDeditText, PWeditText;
     Button button;
-    String id, pw, shakey;
+    String id, pw, shakey, data;
+
     SharedPreferences loginPref;
     Intent intent;
+    private DatabaseReference mDatabase,postRef;// ...
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // ...
+// Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         intent = new Intent(LoginActivity.this, HomeActivity.class);
         IDeditText = (EditText) findViewById(R.id.editid);
         PWeditText = (EditText) findViewById(R.id.editpw);
         button = (Button) findViewById(R.id.button);
+        postRef = FirebaseDatabase.getInstance().getReference().child("MEMBER");
+        mDatabase = FirebaseDatabase.getInstance().getReference(); //database
         loginPref = this.getPreferences( Context.MODE_PRIVATE);
         final SharedPreferences.Editor editor = loginPref.edit();
         String defaultValue = loginPref.getString("login", null);
-        if (defaultValue != null) {
+        if (defaultValue != null) { //자동로그인
             startActivity(intent);
             finish();
         }
@@ -40,15 +71,39 @@ public class LoginActivity extends AppCompatActivity {
                 if ((id.length() * pw.length()) == 0) {
                     Toast.makeText(LoginActivity.this, "Type all info...", Toast.LENGTH_SHORT);
                 } else {
-                    shakey = id + " " + pw;
-                    Log.d("shakey", shakey);
-                    // Save LOGIN information in shared preferences
-                    editor.putString("login", shakey);
-                    editor.commit();
-                    startActivity(intent);
-                    finish();
-                }
+                    postRef.orderByChild("ID").equalTo(id).addListenerForSingleValueEvent(new ValueEventListener() {
+                                @Override
+                                public void onDataChange(DataSnapshot dataSnapshot) {
+                                    if(dataSnapshot.exists()){
+                                        postRef.orderByChild("PW").equalTo(pw).addListenerForSingleValueEvent(new ValueEventListener() {
+                                            @Override
+                                            public void onDataChange(DataSnapshot dataSnapshot) {
+                                                if(dataSnapshot.exists()){
+                                                    shakey = id + " " + pw;
+                                                    Log.d("shakey", shakey);
+                                                    // Save LOGIN information in shared preferences
+                                                    editor.putString("login", shakey);
+                                                    editor.commit();
+                                                    startActivity(intent);
+                                                    finish();
+                                                } else {
+                                                    Toast.makeText(LoginActivity.this, "failed pw", Toast.LENGTH_SHORT);
+                                                }
+                                            }
+                                            @Override
+                                            public void onCancelled(@NonNull DatabaseError databaseError) {
+                                            }
+                                        });
+                                    } else {
+                                        Toast.makeText(LoginActivity.this, "failed id", Toast.LENGTH_SHORT);
+                                    }
+                                }
+                                @Override
+                                public void onCancelled(@NonNull DatabaseError databaseError) {
+                                }
+                });
+
             }
-        });
+        }});
     }
 }
---
+++ app/src/main/java/edu/skku/map/matpwdl/loginuser.java
@@ -0,0 +1,12 @@
+package edu.skku.map.matpwdl;
+
+public class loginuser{
+    public String id, pw;
+    public loginuser(){
+
+    }
+    public loginuser(String id, String pw){
+        this.id = id;
+        this.pw = pw;
+    }
+}
