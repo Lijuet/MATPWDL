9a9f6eedc1db31e4d16f53552f035f2f6b2723e8
fjaoief <38908138+fjaoief@users.noreply.github.com>
2019-06-08 17:05:45 +0900

    member에 room소속 추가

---
+++ app/src/main/java/edu/skku/map/matpwdl/roommembercount.java
@@ -0,0 +1,12 @@
+package edu.skku.map.matpwdl;
+
+public class roommembercount{
+    public String membersize;
+    public roommembercount(){
+
+    }
+    public roommembercount(String membersize){
+        this.membersize = membersize;
+    }
+
+}
