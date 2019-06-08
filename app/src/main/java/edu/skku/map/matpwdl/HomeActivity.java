f6f72026f480b1ce5ccad8d2530a4c3a27f0aa73
sdr04021 <sdr04021@naver.com>
2019-06-08 17:37:18 +0900

    0608/sk/ AddEditRule 수정

--- app/src/main/java/edu/skku/map/matpwdl/HomeActivity.java
+++ app/src/main/java/edu/skku/map/matpwdl/HomeActivity.java
@@ -3,13 +3,21 @@ package edu.skku.map.matpwdl;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.support.annotation.NonNull;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -19,6 +27,8 @@ import org.jsoup.nodes.Element;
 import java.io.IOException;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private Intent serviceIntent;
 
     ConstraintLayout constraint_notice;
     ConstraintLayout constraint_rule;
